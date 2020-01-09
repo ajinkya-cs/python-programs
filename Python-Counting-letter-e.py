db875e2dbb2b8dd86111d7d641001df2bc03e6f0
ajinkya-cs <53271885+ajinkya-cs@users.noreply.github.com>
2020-01-09 16:23:08 +0530

    Add files via upload
    Counting number of times letter E has occured in two strings

---
+++ Python-Counting-letter-e.py
@@ -0,0 +1,11 @@
+x, y = input("Enter two value: ").split()
+count = 0
+  
+for i in x: 
+    if i == 'e': 
+        count = count + 1
+for i in y: 
+    if i == 'e': 
+        count = count + 1        
+   
+print (count) 
