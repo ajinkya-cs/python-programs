c04c83d627628dbaec3dd6f2384a40f84ac2ad64
Ajinkya Muley <ajinkya23muley@gmail.com>
2020-01-30 16:30:21 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,5 @@
+a = int(input('enter a number'))
+fact = 1
+for i in range(1,a+1):
+ fact = fact*i
+print(fact) 
---
+++ list3.py
@@ -0,0 +1,10 @@
+list1 = []
+for i in range (1,11):
+ a = int(input('number to append'))
+ if a == 0:
+     break
+ elif a % 2 == 0:
+    list1.append(a)
+    
+for num in list1:
+    print(num)
